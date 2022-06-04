509e7ee96714faf36b71a3ea37d78c9e6edc3d67
AymanAlshanqiti <ayman.alshanqiti@gmail.com>
2022-06-04 14:16:21 +0300

    build: install storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-	return <div className="text-red-500">Hello</div>;
+	return (
+		<div className="min-h-screen w-screen overflow-x-hidden">Hello There</div>
+	);
 }
 
 export default App;
